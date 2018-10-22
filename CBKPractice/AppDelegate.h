656bf38b44863dd88389ffa790d8cb371453a70b
Stan Liu <lyc2345@gmail.com>
2018-10-22 20:30:45 +0800

    Initial Commit

---
+++ CBKPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CBKPractice
+//
+//  Created by Stan Liu on 2018/10/22.
+//  Copyright © 2018 Stan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CBKPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CBKPractice
+//
+//  Created by Stan Liu on 2018/10/22.
+//  Copyright © 2018 Stan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
