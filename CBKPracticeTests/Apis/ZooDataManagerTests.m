923ad84533a64222e2e3f9d3f37966467985e24b
Stan Liu <lyc2345@gmail.com>
2018-10-27 14:29:56 +0800

    unit test: create ZooDataManagerTests, AnimalTests

---
+++ CBKPracticeTests/Apis/ZooDataManagerTests.m
@@ -0,0 +1,36 @@
+//
+//  ZooDataManagerTests.m
+//  CBKPracticeTests
+//
+//  Created by Stan Liu on 2018/10/27.
+//  Copyright © 2018 Stan Liu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <Kiwi/Kiwi.h>
+#import "ZooDataManager.h"
+
+SPEC_BEGIN(ZooDataManagerTests)
+
+describe(@"ZooDataManager fetch", ^{
+  
+  __block ZooDataManager *manager;
+
+  beforeAll(^{
+    manager = [ZooDataManager shared];
+  });
+
+  it(@"Fetch Data count is not empty", ^{
+    
+    __block NSArray *animalList;
+    
+    [manager fetch:^(NSError * _Nonnull error, NSArray * _Nonnull animals) {
+      [[expectFutureValue(error) shouldEventually] beNil];
+      animalList = animals;
+      [[animals.firstObject should] beKindOfClass: [Animal class]];
+    }];
+    [[expectFutureValue(animalList) shouldEventually] beNonNil];
+  });
+});
+
+SPEC_END
